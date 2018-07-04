602300c89de864fab053da6f274eca680b6aa6d8
ANTE BOZIC <antebozic@gmail.com>
2018-07-04 12:08:08 +0200

    updated css

--- src/components/chart.js
+++ src/components/chart.js
@@ -11,10 +11,10 @@ export default (props) => {
     return (
         <div>
             <Sparklines height={120} width={180} data={props.data}>
-                <SparklinesLine color={props.color} />
+                <SparklinesLine color={props.color}  />
                 <SparklinesReferenceLine type="avg" />
             </Sparklines>
-            <div>{average(props.data)}</div>
+            <div>{average(props.data)} {props.units}</div>
         </div>
     );
 }
